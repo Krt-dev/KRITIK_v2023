6353aff8a898e88fb39111baa5a0068b2b7e9e82
Rhunnan <rhunnandwight.liao.21@usjr.edu.ph>
2023-11-29 04:32:07 +0800

    Final Esttablishment Grid Tiles Done

--- kritik_version_2023/lib/components/establishment_grid.dart
+++ kritik_version_2023/lib/components/establishment_grid.dart
@@ -11,84 +11,75 @@ class EstablishmentsGrid extends StatelessWidget {
     return Container(
       height: 520,
       width: 350,
-      // decoration: BoxDecoration(color: Colors.blue),
-      child: Column(
-        children: [
+      decoration: BoxDecoration(color: Colors.blue),
+      child:
           //text buttons
 
-          Row()
-          //establishments grid View
-          ,
+          //establishments grid V
           GridView.builder(
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              // width / height: fixed for *all* items
-              childAspectRatio: 0.80,
-            ),
-            itemCount: EstablishmentGridData.length,
-            itemBuilder: (context, index) {
-              return Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: SizedBox(
-                  child: Column(
-                    children: [
-                      //establishment Image
-                      Container(
-                          height: 150,
-                          width: 150,
-                          decoration: BoxDecoration(
-                              image: DecorationImage(
-                                  image: AssetImage(
-                                      EstablishmentGridData[index]['path']),
-                                  fit: BoxFit.cover)),
-                          //bookmark and star rating
-                          child: Column(
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.only(
-                                    left: 75.0, top: 7, bottom: 80),
-                                child:
-                                    Image.asset("assets/images/bookmark.png"),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(left: 60),
-                                child:
-                                    Image.asset("assets/images/starRating.png"),
-                              )
-                            ],
-                          )),
-                      //Establishments Name, Location and Location symbol
-                      Padding(
-                        padding: const EdgeInsets.only(left: 10),
-                        child: Row(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          // width / height: fixed for *all* items
+          childAspectRatio: 0.80,
+        ),
+        itemCount: EstablishmentGridData.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: SizedBox(
+              child: Column(
+                children: [
+                  //establishment Image
+                  Container(
+                      height: 150,
+                      width: 150,
+                      decoration: BoxDecoration(
+                          image: DecorationImage(
+                              image: AssetImage(
+                                  EstablishmentGridData[index]['path']),
+                              fit: BoxFit.cover)),
+                      //bookmark and star rating
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(
+                                left: 75.0, top: 7, bottom: 80),
+                            child: Image.asset("assets/images/bookmark.png"),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 60),
+                            child: Image.asset("assets/images/starRating.png"),
+                          )
+                        ],
+                      )),
+                  //Establishments Name, Location and Location symbol
+                  Padding(
+                    padding: const EdgeInsets.only(left: 10),
+                    child: Row(
+                      children: [
+                        Image.asset("assets/images/locationSmallRed.png"),
+                        Column(
                           children: [
-                            Image.asset("assets/images/locationSmallRed.png"),
-                            Column(
-                              children: [
-                                Text(
-                                  EstablishmentGridData[index]['name'],
-                                  style: const TextStyle(
-                                      fontSize: 15,
-                                      fontWeight: FontWeight.w700),
-                                ),
-                                Text(
-                                  EstablishmentGridData[index]['location'],
-                                  style: const TextStyle(
-                                      fontSize: 11,
-                                      fontWeight: FontWeight.w300),
-                                ),
-                              ],
+                            Text(
+                              EstablishmentGridData[index]['name'],
+                              style: const TextStyle(
+                                  fontSize: 15, fontWeight: FontWeight.w700),
+                            ),
+                            Text(
+                              EstablishmentGridData[index]['location'],
+                              style: const TextStyle(
+                                  fontSize: 11, fontWeight: FontWeight.w300),
                             ),
                           ],
                         ),
-                      )
-                    ],
-                  ),
-                ),
-              );
-            },
-          ),
-        ],
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          );
+        },
       ),
     );
   }
